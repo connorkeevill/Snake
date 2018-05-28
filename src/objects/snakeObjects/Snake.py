338bd495cc33de786b341e5af6ebab7e8e4f4289
Connor Keevill <connor.keevill.19@gmail.com>
2018-05-28 21:32:39 +0100

    Make moving snake

--- src/objects/snakeObjects/Snake.py
+++ src/objects/snakeObjects/Snake.py
@@ -9,15 +9,59 @@ class Snake():
         self.direction = 'down'
 
         self.body = []
-        self.spawn(board)
+        self.placeHead(board)
 
-    # | spawn()
-    # |-----------------------------------------
+    # | placeHead()
+    # |----------------------------------------
     # | Creates the snake's "head" and places
-    # | it into the board to begin the game
-    # |---------------------------------
-    def spawn(self, board):
+    # | it into the board to move the snake.
+    # |----------------------------------
+    def placeHead(self, board):
         head = Segment(self.column, self.row)
 
         self.body.append(head)
         board[self.column][self.row].giveItem(head)
+
+    # | move()
+    # |-------------------------------------------
+    # | Moves the snake in its current direction.
+    # |----------------------------------------
+    def move(self, board):
+        self.moveTail(board)
+        self.moveHead()
+        self.placeHead(board)
+
+    # | moveTail()
+    # |--------------------------------------------------------
+    # | Removes the last segment of the snake to prepare for
+    # | the head to move to its next space of the board.
+    # |--------------------------------------------
+    def moveTail(self, board):
+        tailEnd = self.body[-1]
+        board[tailEnd.column][tailEnd.row].emptyContents()
+        self.body.pop(-1)
+
+    # | moveHead()
+    # |-------------------------------------------------------
+    # | Alters the pointer of the snake's position preparing
+    # | to place the head into its new space in the board.
+    # |------------------------------------------------
+    def moveHead(self):
+        if self.direction == 'down':
+            self.row += 1
+        elif self.direction == 'up':
+            self.row -= 1
+        elif self.direction == 'right':
+            self.column += 1
+        elif self.direction == 'left':
+            self.column -= 1
+
+    # | changeDirection()
+    # |------------------------------------------------
+    # | Changes the direction of the snake's movement.
+    # |---------------------------------------------
+    def changeDirection(self, newDirection):
+        directions = ['down', 'up', 'right', 'left']
+
+        if newDirection in directions:
+            self.direction = newDirection
--- src/pages/Play.py
+++ src/pages/Play.py
@@ -24,3 +24,30 @@ class Play(Page):
         self.snake = Snake(snakeColumn, snakeRow, snakeSquares)
 
         self.addToObjects(self.board)
+
+    # | update()
+    # |--------------------------------------------------
+    # | Code to update the page. Essentially code that
+    # | is neither drawing the page, or handling
+    # | events, but code that still needs to
+    # | be ran each loop of the game.
+    # |-------------------------
+    def update(self):
+        self.changeSnakeDirection()
+        self.snake.move(self.board.squares)
+
+    # | changeSnakeDirection()
+    # |----------------------------------------
+    # | Changes the direction the snake moves
+    # | based on the keys that are pressed.
+    # |---------------------------------
+    def changeSnakeDirection(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_DOWN]:
+            self.snake.changeDirection('down')
+        elif keys[pygame.K_UP]:
+            self.snake.changeDirection('up')
+        elif keys[pygame.K_RIGHT]:
+            self.snake.changeDirection('right')
+        elif keys[pygame.K_LEFT]:
+            self.snake.changeDirection('left')
